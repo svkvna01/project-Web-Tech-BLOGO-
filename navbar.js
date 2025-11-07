12fe44fa2581c50968356b8655173d9ec9cb681e
melikenur16 <118025335+b2yza@users.noreply.github.com>
2025-11-08 00:39:49 +0100

    change navbar

--- navbar.js
+++
@@ -1,6 +0,0 @@
-fetch("navbar.html")
-    .then(r => r.text())
-    .then(html => {
-        document.getElementById('topbar-placeholder').innerHTML = html;
-    })
-// will be used later to avoid code duplication
