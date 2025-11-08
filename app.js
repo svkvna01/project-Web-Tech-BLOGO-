5e20044c8733b16c46855c9c906460d2512b1a93
melikenur16 <118025335+b2yza@users.noreply.github.com>
2025-11-08 01:13:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,55 +12,55 @@ app.use(express.static(path.join(__dirname)));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-  secret: 'User',
-  resave: false,
-  saveUninitialized: false,
+    secret: 'User',
+    resave: false,
+    saveUninitialized: false,
 }));
 
 const users = new Map();
 
 app.use((req, res, next) => {
-  res.locals.user = req.session.user || null;
-  next();
+    res.locals.user = req.session.user || null;
+    next();
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/login', (req, res) => {
-  res.render('login');
+    res.render('login');
 });
 
 app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-  const rec = users.get(username);
+    const { username, password } = req.body;
+    const rec = users.get(username);
 
-  if (rec && rec.password === password) {
-    req.session.user = { username, firstName: rec.firstName };
-    return res.redirect('/profile');
-  }
-  return res.redirect('/login');
+    if (rec && rec.password === password) {
+        req.session.user = { username, firstName: rec.firstName };
+        return res.redirect('/profile');
+    }
+    return res.redirect('/login');
 });
 
 app.get('/register', (req, res) => {
-  res.render('register');
+    res.render('register');
 });
 
 app.post('/register', (req, res) => {
-  const { firstName, lastName, email, username, password } = req.body;
+    const { firstName, lastName, email, username, password } = req.body;
 
-  users.set(username, { password, firstName, lastName, email });
-  return res.redirect('/login');
+    users.set(username, { password, firstName, lastName, email });
+    return res.redirect('/login');
 });
 
 function requireAuth(req, res, next) {
-  if (!req.session.user) return res.redirect('/login');
-  next();
+    if (!req.session.user) return res.redirect('/login');
+    next();
 }
 
 app.get('/profile', requireAuth, (req, res) => {
-  res.render('profile', { user: req.session.user });
+    res.render('profile', { user: req.session.user });
 });
 
 const PORT = process.env.PORT || 3000;
