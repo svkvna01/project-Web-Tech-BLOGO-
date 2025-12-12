2c6f5694b47551a132d38fc336595e1bc45f9007
svkvna01 <103519165+svkvna01@users.noreply.github.com>
2025-12-12 12:04:22 +0100

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Benna3maninou26**', //zet hier jouw MYSQL password
+  password: 'ww', //zet hier jouw MYSQL password
   database: 'globo',  //database naam
 });
 
